3870e19e9cd6e59bd80bf38cbb2f32a0272e1069
novi <c5st4wr6ch@live.com>
2022-10-20 10:53:02 +0700

    sample grid

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
-import { Col, Row } from "antd";
+import { Card, Col, Row } from "antd";
 import CardProducts from './components/CardProducts';
 
 import './App.css';
+import './display.less';
+import 'antd/dist/antd.css';
 
 function App() {
   return (
     <div className="App">
       <Row>
-        <Col span={24}>
+        <Col span={12}>
+          <CardProducts />
+        </Col>
+        <Col span={12}>
           <CardProducts />
         </Col>
       </Row>
